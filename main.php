fc4beb927433099b168ded157b2e96c0fc05926c
Sebastian Nunez <sebastian.nunez98@gmail.com>
2018-02-26 16:02:08 -0700

    Creating main home page

---
+++ main.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+  <link rel="stylesheet" href="main.css">
+  <h1 align = "center">Welcome to the Android Music!</h1><p/>
+  <hr>
+  <section align = "left">
+    <p id = "user"> username:
+    <form action = "store.php", method = post, id = "userbutton">
+      <input type = "text" name = "username">
+    </form>
+  </p> <br>
+  <p id = "passkey"> password:
+  <form action = "store.php", method = post, id = "pass">
+    <input type = "text" name = "password">
+  </form>
+  </p> <br>
+  </section>
+
+</body>
+</html>
