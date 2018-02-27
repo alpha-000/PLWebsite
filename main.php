97f69b48acc5f0605ec7a17970f452a7d66858a0
Miguel Angel Nunez <manunez7@miners.utep.edu>
2018-02-26 18:19:09 -0700

    Added new design to Website

--- main.php
+++ main.php
@@ -1,15 +1,16 @@
 <html>
+<img src="andorid_Title.png"  style="width:150px;height:150px;" alt="android_Picture">
 <body>
   <link rel="stylesheet" href="main.css">
-  <h1 align = "center">Welcome to the Android Music!</h1><p/>
+  <h1 align = "left" >Welcome to the Android Music!</h1><p/>
   <hr>
   <section align = "left">
-    <p id = "user"> username:
+    <p id = "user"> Username:
     <form action = "store.php", method = post, id = "userbutton">
       <input type = "text" name = "username">
     </form>
   </p> <br>
-  <p id = "passkey"> password:
+  <p id = "passkey"> Password:
   <form action = "store.php", method = post, id = "pass">
     <input type = "text" name = "password">
   </form>
