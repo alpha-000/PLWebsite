813a505daa37feee853727acb45f673eee44d74f
Sebastian Nunez <sebastian.nunez98@gmail.com>
2018-02-26 18:35:44 -0700

    Store logic

--- store.php
+++ store.php
@@ -5,6 +5,21 @@
   <?php
   $name = $_POST["username"];
   echo $name;
+  $songs = fopen("songs.txt", "r") or die("Refresh browser");
+  echo "<table>"."<tr>"
+       ."<th>".'song'."<th>"
+       ."<th>".'artist'."<th>"
+       ."<th>".'album'."<th>"
+       ."</tr>";
+  while(!feof($songs)){
+       list($s,$at,$ab) = explode(" ", fgets($songs));
+        echo "<tr>"
+        ."<td>".$s."<td>"
+        ."<td>".$at."<td>"
+        ."<td>".$ab."<td>"
+        ."</tr>";
+  }
+    ".</table>"
   ?>
 
 </body>
