55ee058812797c3c9b33944c0aeff9658ef19634
Sebastian Nunez <sebastian.nunez98@gmail.com>
2018-02-26 18:41:54 -0700

    Logic revision

--- main.php
+++ main.php
@@ -3,15 +3,12 @@
 <body>
   <link rel="stylesheet" href="main.css">
   <h1 align = "left" >Welcome to the Android Music!</h1><p/>
-  <hr>
   <section align = "left">
     <p id = "user"> Username:
-    <form action = "store.php", method = post, id = "userbutton">
+    <form action = "store.php" method ="post", id = "userbutton">
       <input type = "text" name = "username">
-    </form>
   </p> </br>
   <p id = "passkey"> Password:
-  <form action = "store.php", method = post, id = "pass">
     <input type = "text" name = "password">
     <input type = "submit">
   </form>
